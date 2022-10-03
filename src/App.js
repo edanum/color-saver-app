2381e2f8c50d649d230b8a0d5106179e41a7034a
Marc Becker <maarc1@gmx.de>
2022-10-04 00:29:05 +0200

    local storage works

--- src/App.js
+++ src/App.js
@@ -4,35 +4,33 @@ import { Card } from "./components/Card";
 import { Form } from "./components/Form";
 import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
-import {colorsArray} from "./data/colors"
+import { colorsArray } from "./data/colors";
 
-const colors = [
-  { id: 1, hexcode: "#000000" },
-  { id: 2, hexcode: "#FF0000" },
-  { id: 3, hexcode: "#00FFFF" },
-  { id: 4, hexcode: "#0F06FF" },
-  { id: 5, hexcode: "#FFF000" },
-  { id: 6, hexcode: "#FFFFFF" },
-];
+
 
 function App() {
-  const [cards, setCards] = useState(JSON.parse(localStorage.getItem("colors"))|| colorsArray );
+  const [colors, setColors] = useState(
+    JSON.parse(localStorage.getItem("colors")) || colorsArray
+  );
 
-  useEffect(() => localStorage.setItem("colors", JSON.stringify(colors)),[colors])
+  useEffect(
+    () => localStorage.setItem("colors", JSON.stringify(colors)),
+    [colors]
+  );
 
   function appendCard(color) {
-    setCards([{ id: nanoid(), hexcode: color.toUpperCase() }, ...cards]);
+    setColors([{ id: nanoid(), hexcode: color.toUpperCase() }, ...colors]);
   }
 
   function deleteCard(id) {
-    setCards(cards.filter((item) => item.id !== id));
+    setColors(colors.filter((item) => item.id !== id));
   }
 
   return (
     <div className="App">
       <Form key={nanoid()} appendCard={appendCard} />
       <div className="cardcontainer">
-        {cards.map((color) => {
+        {colors.map((color) => {
           return (
             <Card
               key={color.id}
