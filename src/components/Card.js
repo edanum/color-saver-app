018290c86ede9dc3b8c99269960463844a8a5313
Marc Becker <maarc1@gmx.de>
2022-10-04 09:20:10 +0200

    colornames added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Card.css";
 
 export function Card({ id, hexcode, deleteCard, editCard }) {
   const [pickedColor, setPickedColor] = useState(hexcode);
+  const [colorName, setColorName] = useState(hexcode);
+
+async function fetchColors(code) {
+  const response = await fetch(
+    `https://www.thecolorapi.com/id?hex=${code.substring(1)}`
+  );
+  const data = await response.json();
+  setColorName(data.name.value);
+}
+
+useEffect(() => {
+  fetchColors(hexcode);
+}, []);
+
+//DIese Funktion ermittelt eine lesbare Farbe in Abnhängigkeit des Hinterfrundes
+  function getContrastYIQ(hexcolor) {
+    hexcolor = hexcolor.replace("#", "");
+    var r = parseInt(hexcolor.substr(0, 2), 16);
+    var g = parseInt(hexcolor.substr(2, 2), 16);
+    var b = parseInt(hexcolor.substr(4, 2), 16);
+    var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+    return yiq >= 128 ? "black" : "white";
+  }
 
   function handleDeleteCard() {
     deleteCard(id);
@@ -24,6 +47,7 @@ export function Card({ id, hexcode, deleteCard, editCard }) {
   return (
     <div className="card" style={{ backgroundColor: pickedColor }}>
       <form className="editform" onSubmit={handleEditCard}>
+        <div style={{ color: getContrastYIQ(pickedColor) }}>{colorName}</div>
         <button
           type="submit"
           className="card__hexcode"
